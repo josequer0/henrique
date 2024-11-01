c514090c551d7ff0f6fcadd2453420674fc3b03e
josequer0 <167535917+josequer0@users.noreply.github.com>
2024-11-01 11:03:22 -0300

    Add files via upload

---
+++ jose/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'dropshipping',
+    'O que é dropshipping?',
+    'Dropshipping e quando voce vende produtos sem ter estoque'
+)
+
+criaCartao(
+    'trafego pago',
+    'o que é trafego pago?',
+    'trafego pago e quando voce paga para que promovam seu site, produto ou serviço'
+)
+
+criaCartao(
+    'marketing digital]',
+    'O que é o marketig digital?',
+    'marketing digital é a promoção de produtos, seriços ou marca usando canais e ferramentas digitai como redes sociais '
+)
+
+criaCartao(
+    'plataformas para vendas online',
+    'qual as melhores plataforma?',
+    'kirvano, kiwify, hotmart'
+)
